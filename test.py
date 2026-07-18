ccdd74749913bc98627e1ee7a5fb38205b1b8db7
Abhik Khan <abhikkhan00@gmail.com>
2026-07-18 17:25:11 +0530

    implemented and tested flight tool

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
 
-res = tavily_search("Best hotels in India")
-print(res)
+#res = tavily_search("Best hotels in India")
+#rint(res)
+
+result = search_flights("Plan a 7 days england trip from India")
+print(result)
